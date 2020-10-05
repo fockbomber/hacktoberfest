b5751500b76f9f63614f37b89ffaf60e4eb4ab5b
Achint04012001 <56585189+Achint04012001@users.noreply.github.com>
2020-10-05 09:15:20 +0530

    Create DES.py

---
+++ DES.py
@@ -0,0 +1,26 @@
+#DES CIPHER 
+
+from Crypto.Cipher import DES
+
+from Crypto.Util.Padding import pad, unpad
+
+#key
+key = b"12345678"
+pt = b"1"
+
+crypto_object = DES.new(key,DES.MODE_ECB)
+
+ct = crypto_object.encrypt(pad(pt,DES.block_size))
+
+print("Encrypted text is : ",ct)
+print("Key Size: ",len(key)," bytes","aka ",len(key*8)," bits")
+
+#decrypting text
+
+pt = unpad(crypto_object.decrypt(ct),DES.block_size)
+print("The plain text is : ",pt)
+
+
+#DES CIPHER ENDS HERE 
+
+
